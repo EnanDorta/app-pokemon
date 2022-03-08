b29ff134599212a1655f27bd47d581f56868f048
Samir Santos <santossamir@hotmail.com>
2022-03-07 23:10:53 -0300

    Adicionando pastas no projeto.

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppPrincipal from './page/AppPrincipal';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+  
+    <AppPrincipal/>
+    ,root
+);
+
